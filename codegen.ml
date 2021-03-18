541ba9875ca83c11b2192cb5b39895e5cbc6d50c
mwinitch <michaelwin26@gmail.com>
2021-03-18 23:48:11 +0000

    Initial commit

---
+++ codegen.ml
@@ -0,0 +1,21 @@
+module L = Llvm
+module A = Ast
+
+module StringMap = Map.Make(String)
+
+let translate (globals, functions) =
+  let context = L.global_context () in
+  let the_module = L.create_module context "MicroC" (*Maybe change this name to Bugsy*)
+  and i32_t  = L.i32_type  context
+  and i8_t   = L.i8_type   context
+  and i1_t   = L.i1_type   context
+  and void_t = L.void_type context in
+
+  let ltype_of_typ = function
+      A.Num -> i32_t
+    | A.Bool -> i1_t
+    | A.Void -> void_t in
+
+  (* Declare printf(), which the print built-in function will call *)
+  let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let printf_func = L.declare_function "printf" printf_t the_module in
