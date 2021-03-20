82376fc7cf240c6253615b2fe337e78eaa944524
Jason <jasoncardinale19@gmail.com>
2021-03-20 19:44:19 -0400

    semant compiles

---
+++ sast.ml
@@ -0,0 +1,106 @@
+(* Semantically-checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+type sexpr = typ * sx
+and sx =
+    SLiteral of int
+  (* | SFliteral of string *)
+  | SBoolLit of bool
+  | SId of string
+  | SBinop of sexpr * op * sexpr
+  | SUnop of uop * sexpr
+  | SAssign of string * sexpr
+  | SCall of string * sexpr list
+  | SNoexpr
+
+type sstmt =
+    SBlock of sstmt list
+  | SExpr of sexpr
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sexpr * sstmt
+  | SWhile of sexpr * sstmt
+
+  type sconstruct_decl = {
+    sctformals : bind list;
+    sctlocals : bind list;
+    sctbody : sstmt list;
+  }
+
+type sfunc_decl = {
+    styp : typ;
+    sfname : string;
+    sformals : bind list;
+    slocals : bind list;
+    sbody : sstmt list;
+  }
+
+  type scdecl = {
+    scname : string;
+    scdvars : bind list;
+    scdconst: sconstruct_decl list;
+    scdfuncs: sfunc_decl list;
+    }
+
+type sprogram = bind list * sfunc_decl list
+
+(* Pretty-printing functions *)
+
+let rec string_of_sexpr (t, e) =
+  "(" ^ string_of_typ t ^ " : " ^ (match e with
+    SLiteral(l) -> string_of_int l
+  | SBoolLit(true) -> "true"
+  | SBoolLit(false) -> "false"
+  (* | SFliteral(l) -> l *)
+  | SId(s) -> s
+  | SBinop(e1, o, e2) ->
+      string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+  | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | SCall(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SNoexpr -> ""
+				  ) ^ ")"
+
+let rec string_of_sstmt = function
+    SBlock(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
+  | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
+  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
+  | SIf(e, s, SBlock([])) ->
+      "if (" ^ string_of_sexpr e ^ ")\n" ^ string_of_sstmt s
+  | SIf(e, s1, s2) ->  "if (" ^ string_of_sexpr e ^ ")\n" ^
+      string_of_sstmt s1 ^ "else\n" ^ string_of_sstmt s2
+  | SFor(e1, e2, e3, s) ->
+      "for (" ^ string_of_sexpr e1  ^ " ; " ^ string_of_sexpr e2 ^ " ; " ^
+      string_of_sexpr e3  ^ ") " ^ string_of_sstmt s
+  | SWhile(e, s) -> "while (" ^ string_of_sexpr e ^ ") " ^ string_of_sstmt s
+
+
+  let string_of_sconst_decl const_decl =
+    "constructor(" ^ String.concat ", " (List.map snd const_decl.sctformals) ^
+    ")\n{\n" ^
+    String.concat "" (List.map string_of_vdecl const_decl.sctlocals) ^
+    String.concat "" (List.map string_of_sstmt const_decl.sctbody) ^
+    "}\n"
+
+let string_of_sfdecl fdecl =
+  string_of_typ fdecl.styp ^ " " ^
+  fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
+  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
+  "}\n"
+
+  let string_of_scdecl cdecl =
+    cdecl.scname ^ "{" ^
+    String.concat "" (List.map string_of_vdecl cdecl.scdvars) ^
+    String.concat "" (List.map string_of_sconst_decl cdecl.scdconst) ^
+    String.concat "" (List.map string_of_sfdecl cdecl.scdfuncs) ^
+    "}\n"
+
+let string_of_sprogram (vars, funcs, _classes) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "" (List.map string_of_scdecl _classes) ^ "\n" ^
+  String.concat "\n" (List.map string_of_sfdecl funcs)
