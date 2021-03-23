5fcb5d0a4c3cb984f1fbb49c29d07aadd3d31919
evantilley <elt2141@columbia.edu>
2021-03-23 03:39:46 +0000

    printing strings working!

--- semant.ml
+++ semant.ml
@@ -38,10 +38,10 @@ let check (globals, functions, classes) =
       fname = name;
       formals = [(ty, "x")];
       locals = []; body = [] } map
-    in List.fold_left add_bind StringMap.empty [ ("print", Num);
+    in List.fold_left add_bind StringMap.empty [ 
 			                         ("printb", Bool);
-			                         ("printf", Num);
-                                     ("print", String)
+			                         ("print", Num);
+                                     ("printf", String);
 			                         ("printbig", Num) ]
   in
 
--- test.mc
+++ test.mc
@@ -2,13 +2,15 @@ num main() {
     num x;
     num y;
     num z;
-    string x;
+    string a;
+    a = "hello world";
+    printf(a);
     x = 4.7;
     y = 5.6;
     z = 8.0;
     z = x + y;
-    printf(z);
-   printf(y);
+  /*  printf(z); */
+  /*  printf(y); */
     return 24.9;
 }
 
