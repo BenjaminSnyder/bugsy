1027d2aa619db1143b087620609f7ca888785d78
Ben <bs3148@columbia.edu>
2021-03-28 22:38:57 +0000

    changed gitignore and updated demo compile script

--- demo_test.sh
+++ demo_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./bugsy.native demo.mc > "demo.ll"
+./bugsy.native demo.bug > "demo.ll"
 llc "demo.ll" -o "demo.s"
 gcc "builtins.c" -c -o "builtins.o"
 g++ "demo.s" "builtins.o" -lglut -lGL -lGLU -lGLEW -o demo
